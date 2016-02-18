cbab59ca879f04c2ba6abfa38309985411df0516
Jan Hermann <jh@janhermann.cz>
2016-02-18 18:03:08 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, abort, request
+import json
+from pathlib import Path
+
+app = Flask(__name__)
+db_path = app.root_path + '/db.json'
+if not Path(db_path).is_file():
+    with open(db_path, 'w') as f:
+        json.dump({}, f)
+
+
+@app.route('/submit/<id>', methods=['POST'])
+def submit(id):
+    if request.method == 'POST':
+        with open(db_path) as f:
+            db = json.load(f)
+        if id in db:
+            abort(404)
+        db[id] = request.get_data().decode().split()
+        with open(db_path, 'w') as f:
+            json.dump(db, f)
+    return ''
+
+
+@app.route('/get/<id>')
+def get(id):
+    with open(db_path) as f:
+        db = json.load(f)
+    if id not in db:
+        abort(404)
+    task = db[id].pop(0)
+    if not db[id]:
+        del db[id]
+    with open(db_path, 'w') as f:
+        json.dump(db, f)
+    return task
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
