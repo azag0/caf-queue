0a6f1d330381d4065119b8505f12805812223b63
Jan Hermann <dev@hermann.in>
2017-05-22 21:21:07 +0200

    add MPL license

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from flask import Flask, request, url_for, render_template, session, \
     abort, redirect
 from flask_sqlalchemy import SQLAlchemy
